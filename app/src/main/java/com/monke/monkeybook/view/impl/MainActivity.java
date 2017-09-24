aca49755f1c15f7efc36e0e07a47423f8eecf94d
1105075896@qq.com <zqh19931118>
2017-09-25 00:59:00 +0800

    c

--- app/src/main/java/com/monke/monkeybook/view/impl/MainActivity.java
+++ app/src/main/java/com/monke/monkeybook/view/impl/MainActivity.java
@@ -65,8 +65,6 @@ public class MainActivity extends MBaseActivity<IMainPresenter> implements IMain
 
     @Override
     protected void bindView() {
-        int d = 1;
-
         downloadListPop = new DownloadListPop(MainActivity.this);
 
         rfRvShelf = (RefreshRecyclerView) findViewById(R.id.rf_rv_shelf);
