3eb0d2a4cb5042f97e51bf5c12b2ace6eb15ae40
GKF <gekunfei@live.com>
2017-12-13 10:44:04 +0800

    菜单添加图标

--- app/src/main/java/com/monke/monkeybook/base/MBaseActivity.java
+++ app/src/main/java/com/monke/monkeybook/base/MBaseActivity.java
@@ -1,9 +1,13 @@
 //Copyright (c) 2017. 章钦豪. All rights reserved.
 package com.monke.monkeybook.base;
 
+import android.view.Menu;
+
 import com.monke.basemvplib.IPresenter;
 import com.monke.basemvplib.impl.BaseActivity;
 import com.umeng.analytics.MobclickAgent;
+
+import java.lang.reflect.Method;
 
 public abstract class MBaseActivity<T extends IPresenter> extends BaseActivity<T>{
     @Override
@@ -16,5 +20,21 @@ public abstract class MBaseActivity<T extends IPresenter> extends BaseActivity<T
     protected void onPause() {
         super.onPause();
         MobclickAgent.onPause(this);
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (menu != null) {
+            if (menu.getClass().getSimpleName().equalsIgnoreCase("MenuBuilder")) {
+                try {
+                    Method method = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    method.setAccessible(true);
+                    method.invoke(menu, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
     }
 }
