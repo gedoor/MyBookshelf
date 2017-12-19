23becc98b3c85c7eb3c1bc59e5b59613d96fa8bb
gedoor <gekunfei@live.com>
2017-12-19 21:14:40 +0800

    update

--- app/src/main/java/com/monke/monkeybook/MApplication.java
+++ app/src/main/java/com/monke/monkeybook/MApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+
 import com.monke.monkeybook.service.DownloadService;
 
 public class MApplication extends Application {
@@ -15,17 +16,11 @@ public class MApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (BuildConfig.IS_RELEASE) {
-            String channel = "debug";
-            try {
-                ApplicationInfo appInfo = getPackageManager()
-                        .getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
-                channel = appInfo.metaData.getString("UMENG_CHANNEL_VALUE");
-                versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
-            } catch (PackageManager.NameNotFoundException e) {
-                versionName = "0.0.0";
-                e.printStackTrace();
-            }
+        try {
+            versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            versionName = "0.0.0";
+            e.printStackTrace();
         }
         instance = this;
         startService(new Intent(this, DownloadService.class));
