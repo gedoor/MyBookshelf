1a607f25e836a1291856517b6daae004c9d7c2c6
GKF <gekunfei@live.com>
2017-12-19 13:59:44 +0800

    屏幕方向设置

--- app/src/main/java/com/monke/monkeybook/base/MBaseActivity.java
+++ app/src/main/java/com/monke/monkeybook/base/MBaseActivity.java
@@ -1,14 +1,28 @@
 //Copyright (c) 2017. 章钦豪. All rights reserved.
 package com.monke.monkeybook.base;
 
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 
 import com.monke.basemvplib.IPresenter;
 import com.monke.basemvplib.impl.BaseActivity;
+import com.monke.monkeybook.R;
 
 import java.lang.reflect.Method;
 
-public abstract class MBaseActivity<T extends IPresenter> extends BaseActivity<T>{
+public abstract class MBaseActivity<T extends IPresenter> extends BaseActivity<T> {
+    public SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        setOrientation();
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -33,5 +47,22 @@ public abstract class MBaseActivity<T extends IPresenter> extends BaseActivity<T
             }
         }
         return super.onMenuOpened(featureId, menu);
+    }
+
+    private void setOrientation() {
+        switch (preferences.getString(getString(R.string.pk_screen_direction), "0")) {
+            case "0":
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                break;
+            case "1":
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                break;
+            case "2":
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+            case "3":
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+                break;
+        }
     }
 }
