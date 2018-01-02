d618d1035a0a8c6c7c35b479e1a8b4969bdd4065
GKF <gekunfei@live.com>
2018-01-02 09:15:35 +0800

    update

--- app/src/main/java/com/monke/monkeybook/presenter/impl/ReadBookPresenterImpl.java
+++ app/src/main/java/com/monke/monkeybook/presenter/impl/ReadBookPresenterImpl.java
@@ -218,6 +218,7 @@ public class ReadBookPresenterImpl extends BasePresenterImpl<IBookReadView> impl
                     List<BookContentBean> tempList = DbHelper.getInstance().getmDaoSession().getBookContentBeanDao().queryBuilder()
                             .where(BookContentBeanDao.Properties.DurChapterUrl.eq(bookShelf.getBookInfoBean()
                                     .getChapterlist().get(chapterIndex).getDurChapterUrl())).build().list();
+                    //加载下一章节
                     LoadNextChapter(chapterIndex);
                     e.onNext(new ReadBookContentBean(tempList == null ? new ArrayList<BookContentBean>() : tempList, finalPageIndex1));
                     e.onComplete();
