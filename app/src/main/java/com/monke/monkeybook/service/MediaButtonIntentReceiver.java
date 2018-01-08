0726bef1272cd2e6ba1ec0d2edb4882b5313b726
GKF <gekunfei@live.com>
2018-01-08 08:07:22 +0800

    update

--- app/src/main/java/com/monke/monkeybook/service/MediaButtonIntentReceiver.java
+++ app/src/main/java/com/monke/monkeybook/service/MediaButtonIntentReceiver.java
@@ -25,42 +25,6 @@ public class MediaButtonIntentReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = BuildConfig.DEBUG;
     public static final String TAG = MediaButtonIntentReceiver.class.getSimpleName();
 
-    private static final int MSG_HEADSET_DOUBLE_CLICK_TIMEOUT = 2;
-
-    private static final int DOUBLE_CLICK = 400;
-
-    private static int mClickCounter = 0;
-    private static long mLastClickTime = 0;
-
-    @SuppressLint("HandlerLeak") // false alarm, handler is already static
-    private static Handler mHandler = new Handler() {
-
-        @Override
-        public void handleMessage(final Message msg) {
-            switch (msg.what) {
-                case MSG_HEADSET_DOUBLE_CLICK_TIMEOUT:
-                    final int clickCount = msg.arg1;
-                    final String command;
-
-                    if (DEBUG) Log.v(TAG, "Handling headset click, count = " + clickCount);
-                    switch (clickCount) {
-                        case 1:
-                            command = ReadAloudService.mediaButtonAction;
-                            break;
-                        default:
-                            command = null;
-                            break;
-                    }
-
-                    if (command != null) {
-                        final Context context = (Context) msg.obj;
-                        startService(context, command);
-                    }
-                    break;
-            }
-        }
-    };
-
     @Override
     public void onReceive(final Context context, final Intent intent) {
         if (DEBUG) Log.v(TAG, "Received intent: " + intent);
@@ -78,8 +42,6 @@ public class MediaButtonIntentReceiver extends BroadcastReceiver {
             }
 
             final int keycode = event.getKeyCode();
-            final int action = event.getAction();
-            final long eventTime = event.getEventTime();
 
             String command = null;
             switch (keycode) {
@@ -91,40 +53,10 @@ public class MediaButtonIntentReceiver extends BroadcastReceiver {
                     break;
             }
             if (command != null) {
-                if (action == KeyEvent.ACTION_DOWN) {
-                    if (event.getRepeatCount() == 0) {
-                        // Only consider the first event in a sequence, not the repeat events,
-                        // so that we don't trigger in cases where the first event went to
-                        // a different app (e.g. when the user ends a phone call by
-                        // long pressing the headset button)
-
-                        // The service may or may not be running, but we need to send it
-                        // a command.
-                        if (keycode == KeyEvent.KEYCODE_HEADSETHOOK) {
-                            if (eventTime - mLastClickTime >= DOUBLE_CLICK) {
-                                mClickCounter = 0;
-                            }
-
-                            mClickCounter++;
-                            if (DEBUG) Log.v(TAG, "Got headset click, count = " + mClickCounter);
-                            mHandler.removeMessages(MSG_HEADSET_DOUBLE_CLICK_TIMEOUT);
-
-                            Message msg = mHandler.obtainMessage(
-                                    MSG_HEADSET_DOUBLE_CLICK_TIMEOUT, mClickCounter, 0, context);
-
-                            long delay = mClickCounter < 3 ? DOUBLE_CLICK : 0;
-                            if (mClickCounter >= 3) {
-                                mClickCounter = 0;
-                            }
-                            mLastClickTime = eventTime;
-                        } else {
-                            startService(context, command);
-                        }
-                        return true;
-                    }
+                startService(context, command);
+                return true;
                 }
             }
-        }
         return false;
     }
 
