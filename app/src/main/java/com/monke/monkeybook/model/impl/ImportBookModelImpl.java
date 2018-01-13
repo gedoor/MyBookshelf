63c6fc836c042bfecabcaacdedfa61699231d4c0
gedoor <gekunfei@live.com>
2018-01-13 14:52:53 +0800

    优化导入txt格式

--- app/src/main/java/com/monke/monkeybook/model/impl/ImportBookModelImpl.java
+++ app/src/main/java/com/monke/monkeybook/model/impl/ImportBookModelImpl.java
@@ -9,8 +9,11 @@ import com.monke.monkeybook.dao.BookInfoBeanDao;
 import com.monke.monkeybook.dao.BookShelfBeanDao;
 import com.monke.monkeybook.dao.ChapterListBeanDao;
 import com.monke.monkeybook.dao.DbHelper;
+import com.monke.monkeybook.help.FormatWebText;
 import com.monke.monkeybook.model.IImportBookModel;
+
 import org.mozilla.universalchardet.UniversalDetector;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,6 +24,7 @@ import java.security.MessageDigest;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -45,10 +49,10 @@ public class ImportBookModelImpl extends BaseModelImpl implements IImportBookMod
             in = null;
 
             String md5 = new BigInteger(1, md.digest()).toString(16);
-            BookShelfBean bookShelfBean = null;
+            BookShelfBean bookShelfBean;
             List<BookShelfBean> temp = DbHelper.getInstance().getmDaoSession().getBookShelfBeanDao().queryBuilder().where(BookShelfBeanDao.Properties.NoteUrl.eq(md5)).build().list();
             Boolean isNew = true;
-            if (temp!=null && temp.size()>0) {
+            if (temp != null && temp.size() > 0) {
                 isNew = false;
                 bookShelfBean = temp.get(0);
                 bookShelfBean.setBookInfoBean(DbHelper.getInstance().getmDaoSession().getBookInfoBeanDao().queryBuilder().where(BookInfoBeanDao.Properties.NoteUrl.eq(bookShelfBean.getNoteUrl())).build().list().get(0));
@@ -72,7 +76,7 @@ public class ImportBookModelImpl extends BaseModelImpl implements IImportBookMod
                 DbHelper.getInstance().getmDaoSession().getBookShelfBeanDao().insertOrReplace(bookShelfBean);
             }
             bookShelfBean.getBookInfoBean().setChapterlist(DbHelper.getInstance().getmDaoSession().getChapterListBeanDao().queryBuilder().where(ChapterListBeanDao.Properties.NoteUrl.eq(bookShelfBean.getNoteUrl())).orderAsc(ChapterListBeanDao.Properties.DurChapterIndex).build().list());
-            e.onNext(new LocBookShelfBean(isNew,bookShelfBean));
+            e.onNext(new LocBookShelfBean(isNew, bookShelfBean));
             e.onComplete();
         });
     }
@@ -123,15 +127,16 @@ public class ImportBookModelImpl extends BaseModelImpl implements IImportBookMod
         BufferedReader buffreader = new BufferedReader(inputreader);
         String line;
         while ((line = buffreader.readLine()) != null) {
+            line = FormatWebText.getContent(line);
             Pattern p = Pattern.compile(regex);
             Matcher m = p.matcher(line);
             if (m.find()) {
-                String temp = line.trim().substring(0,line.trim().indexOf("第"));
-                if(temp!= null && temp.trim().length()>0){
+                String temp = line.trim().substring(0, line.trim().indexOf("第"));
+                if (temp != null && temp.trim().length() > 0) {
                     contentBuilder.append(temp);
                 }
                 if (contentBuilder.toString().length() > 0) {
-                    if(contentBuilder.toString().replaceAll("　","").trim().length()>0){
+                    if (contentBuilder.toString().replaceAll("　", "").trim().length() > 0) {
                         saveDurChapterContent(md5, chapterPageIndex, title, contentBuilder.toString());
                         chapterPageIndex++;
                     }
@@ -139,17 +144,13 @@ public class ImportBookModelImpl extends BaseModelImpl implements IImportBookMod
                 }
                 title = line.trim().substring(line.trim().indexOf("第"));
             } else {
-                if (line.trim().length() == 0) {
-                    if (contentBuilder.length() > 0) {
-                        contentBuilder.append("\r\n\u3000\u3000");
-                    } else {
-                        contentBuilder.append("\r\u3000\u3000");
-                    }
+                if (contentBuilder.length() > 0) {
+                    contentBuilder.append("\r\n\u3000\u3000").append(line);
                 } else {
-                    contentBuilder.append(line);
-                    if (title == null) {
-                        title = line.trim();
-                    }
+                    contentBuilder.append("\r\u3000\u3000").append(line);
+                }
+                if (title == null) {
+                    title = line;
                 }
             }
         }
