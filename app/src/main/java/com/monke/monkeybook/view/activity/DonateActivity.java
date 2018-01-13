73ecac0552fb2fa4229d91148d4c228d885255bc
gedoor <gekunfei@live.com>
2018-01-13 11:09:18 +0800

    捐赠页面

---
+++ app/src/main/java/com/monke/monkeybook/view/activity/DonateActivity.java
@@ -0,0 +1,28 @@
+package com.monke.monkeybook.view.activity;
+
+import com.monke.basemvplib.IPresenter;
+import com.monke.monkeybook.base.MBaseActivity;
+
+/**
+ * Created by GKF on 2018/1/13.
+ * 捐赠页面
+ */
+
+public class DonateActivity extends MBaseActivity {
+
+
+    @Override
+    protected IPresenter initInjector() {
+        return null;
+    }
+
+    @Override
+    protected void onCreateActivity() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
