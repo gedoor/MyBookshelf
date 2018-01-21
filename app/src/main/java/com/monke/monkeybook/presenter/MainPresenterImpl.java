f75252a4be91462bb02a370acaf18c84e71df380
gedoor <gekunfei@live.com>
2018-01-21 21:45:26 +0800

    添加网址

--- app/src/main/java/com/monke/monkeybook/presenter/MainPresenterImpl.java
+++ app/src/main/java/com/monke/monkeybook/presenter/MainPresenterImpl.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.provider.DocumentFile;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.google.gson.reflect.TypeToken;
@@ -19,11 +20,13 @@ import com.monke.monkeybook.R;
 import com.monke.monkeybook.base.observer.SimpleObserver;
 import com.monke.monkeybook.bean.BookInfoBean;
 import com.monke.monkeybook.bean.BookShelfBean;
+import com.monke.monkeybook.bean.BookSourceBean;
 import com.monke.monkeybook.common.RxBusTag;
 import com.monke.monkeybook.dao.BookInfoBeanDao;
 import com.monke.monkeybook.dao.BookShelfBeanDao;
 import com.monke.monkeybook.dao.ChapterListBeanDao;
 import com.monke.monkeybook.dao.DbHelper;
+import com.monke.monkeybook.help.BookShelf;
 import com.monke.monkeybook.help.FileHelper;
 import com.monke.monkeybook.listener.OnGetChapterListListener;
 import com.monke.monkeybook.model.WebBookModelImpl;
@@ -31,11 +34,16 @@ import com.monke.monkeybook.presenter.impl.IMainPresenter;
 import com.monke.monkeybook.utils.NetworkUtil;
 import com.monke.monkeybook.view.impl.IMainView;
 
+import org.jsoup.select.Evaluator;
+
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
@@ -172,7 +180,88 @@ public class MainPresenterImpl extends BasePresenterImpl<IMainView> implements I
 
     @Override
     public void addBookUrl(String bookUrl) {
+        if (TextUtils.isEmpty(bookUrl.trim())) return;
+        Observable.create((ObservableOnSubscribe<BookShelfBean>) e -> {
+            URL url = new URL(bookUrl);
+            List<BookInfoBean> temp = DbHelper.getInstance().getmDaoSession().getBookInfoBeanDao().queryBuilder()
+                    .where(BookInfoBeanDao.Properties.NoteUrl.eq(bookUrl.toString())).limit(1).build().list();
+            if (temp != null && temp.size() > 0) {
+                e.onNext(null);
+            } else {
+                BookShelfBean bookShelfBean = new BookShelfBean();
+                bookShelfBean.setTag(String.format("%s://%s", url.getProtocol(), url.getHost()));
+                bookShelfBean.setNoteUrl(url.toString());
+                bookShelfBean.setDurChapter(0);
+                bookShelfBean.setDurChapterPage(0);
+                bookShelfBean.setFinalDate(0);
+                e.onNext(bookShelfBean);
+            }
+            e.onComplete();
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SimpleObserver<BookShelfBean>() {
+                    @Override
+                    public void onNext(BookShelfBean bookShelfBean) {
+                        if (bookShelfBean != null) {
+                            getBook(bookShelfBean);
+                        } else {
+                            Toast.makeText(mView.getContext(), "已在书架中", Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(mView.getContext(), "网址格式不对", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void getBook(BookShelfBean bookShelfBean) {
+        WebBookModelImpl.getInstance().getBookInfo(bookShelfBean).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SimpleObserver<BookShelfBean>() {
+                    @Override
+                    public void onNext(BookShelfBean value) {
+                        WebBookModelImpl.getInstance().getChapterList(value, new OnGetChapterListListener() {
+                            @Override
+                            public void success(BookShelfBean bookShelfBean) {
+                                saveBookToShelf(bookShelfBean);
+                            }
+
+                            @Override
+                            public void error() {
+                                Toast.makeText(mView.getContext(), "获取书籍目录失败", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(mView.getContext(), "获取书籍信息失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void saveBookToShelf(BookShelfBean bookShelfBean) {
+        Observable.create((ObservableOnSubscribe<BookShelfBean>) e -> {
+            BookShelf.saveBookToShelf(bookShelfBean);
+            e.onNext(bookShelfBean);
+            e.onComplete();
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SimpleObserver<BookShelfBean>() {
+                    @Override
+                    public void onNext(BookShelfBean bookShelfBean) {
+                        //成功   //发送RxBus
+                        RxBus.get().post(RxBusTag.HAD_ADD_BOOK, bookShelfBean);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(mView.getContext(), "保存书籍失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void startRefreshBook(List<BookShelfBean> value) {
@@ -204,16 +293,16 @@ public class MainPresenterImpl extends BasePresenterImpl<IMainView> implements I
         }
     }
     //保存更新
-    private void saveBookToShelf(final List<BookShelfBean> datas, final int index, final boolean hasUpdate) {
+    private void saveBookToShelf(final List<BookShelfBean> dataS, final int index, final boolean hasUpdate) {
         Observable.create((ObservableOnSubscribe<BookShelfBean>) e -> {
-            DbHelper.getInstance().getmDaoSession().getChapterListBeanDao().insertOrReplaceInTx(datas.get(index).getBookInfoBean().getChapterList());
+            DbHelper.getInstance().getmDaoSession().getChapterListBeanDao().insertOrReplaceInTx(dataS.get(index).getBookInfoBean().getChapterList());
             if (hasUpdate) {
-                datas.get(index).setHasUpdate(true);
-                datas.get(index).getBookInfoBean().setFinalRefreshData(System.currentTimeMillis());
-                DbHelper.getInstance().getmDaoSession().getBookInfoBeanDao().insertOrReplace(datas.get(index).getBookInfoBean());
-                DbHelper.getInstance().getmDaoSession().getBookShelfBeanDao().insertOrReplace(datas.get(index));
+                dataS.get(index).setHasUpdate(true);
+                dataS.get(index).getBookInfoBean().setFinalRefreshData(System.currentTimeMillis());
+                DbHelper.getInstance().getmDaoSession().getBookInfoBeanDao().insertOrReplace(dataS.get(index).getBookInfoBean());
+                DbHelper.getInstance().getmDaoSession().getBookShelfBeanDao().insertOrReplace(dataS.get(index));
             }
-            e.onNext(datas.get(index));
+            e.onNext(dataS.get(index));
             e.onComplete();
         })
                 .subscribeOn(Schedulers.io())
@@ -222,7 +311,7 @@ public class MainPresenterImpl extends BasePresenterImpl<IMainView> implements I
                     @Override
                     public void onNext(BookShelfBean value) {
                         mView.refreshRecyclerViewItemAdd();
-                        refreshBookShelf(datas, index + 1);
+                        refreshBookShelf(dataS, index + 1);
                     }
 
                     @Override
