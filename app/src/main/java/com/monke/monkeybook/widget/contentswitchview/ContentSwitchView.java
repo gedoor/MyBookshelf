a7da5cb98daffef1bc9d4bd3c3f49948231e4677
GKF <gekunfei@live.com>
2018-02-11 17:06:55 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/widget/contentswitchview/ContentSwitchView.java
+++ app/src/main/java/com/monke/monkeybook/widget/contentswitchview/ContentSwitchView.java
@@ -14,7 +14,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.monke.monkeybook.help.ReadBookControl;
 import com.monke.monkeybook.utils.DensityUtil;
