8314d1aa61b7540b6f13d2f16dfbc4b52f51601c
GKF <gekunfei@live.com>
2018-03-01 16:36:19 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/model/impl/IHttpGetApi.java
+++ app/src/main/java/com/monke/monkeybook/model/impl/IHttpGetApi.java
@@ -16,12 +16,12 @@ import retrofit2.http.Url;
  */
 
 public interface IHttpGetApi {
-    @GET("{path}")
+    @GET()
     @Headers({"Accept:text/html,application/xhtml+xml,application/xml",
             "User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
             "Keep-Alive:300",
             "Connection:Keep-Alive"})
-    Observable<String> getWebContent(@Path("path") String path);
+    Observable<String> getWebContent(@Url String url);
 
     @GET()
     @Headers({"Accept:text/html,application/xhtml+xml,application/xml",
