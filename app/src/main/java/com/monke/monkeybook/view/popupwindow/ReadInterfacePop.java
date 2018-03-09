b7d029988ea511d9562b5e9ead638a67f7599735
GKF <gekunfei@live.com>
2018-03-09 11:25:56 +0800

    自定义背景

--- app/src/main/java/com/monke/monkeybook/view/popupwindow/ReadInterfacePop.java
+++ app/src/main/java/com/monke/monkeybook/view/popupwindow/ReadInterfacePop.java
@@ -192,8 +192,6 @@ public class ReadInterfacePop extends PopupWindow {
     }
 
     private void updateBg(int index) {
-        civBgCustom.getDrawable().mutate();
-        civBgCustom.getDrawable().setColorFilter(activity.getResources().getColor(R.color.tv_text_default), PorterDuff.Mode.SRC_ATOP);
         civBgWhite.setBorderColor(activity.getResources().getColor(R.color.tv_text_default));
         civBgYellow.setBorderColor(activity.getResources().getColor(R.color.tv_text_default));
         civBgGreen.setBorderColor(activity.getResources().getColor(R.color.tv_text_default));
