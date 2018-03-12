9e7a15cc5458a8aca5c994b00b0f696a921231b0
GKF <gekunfei@live.com>
2018-03-12 14:52:47 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/view/activity/ReplaceRuleActivity.java
+++ app/src/main/java/com/monke/monkeybook/view/activity/ReplaceRuleActivity.java
@@ -120,7 +120,6 @@ public class ReplaceRuleActivity extends MBaseActivity {
         setupActionBar();
         initRecyclerView();
         moProgressHUD = new MoProgressHUD(this);
-        initRecyclerView();
     }
 
     @Override
