7ceaa3d9f5995dc77f0ddaa8312d93173adf2b93
GKF <gekunfei@live.com>
2018-04-08 16:59:22 +0800

    优化替换

--- app/src/main/java/com/monke/monkeybook/presenter/ReadBookPresenterImpl.java
+++ app/src/main/java/com/monke/monkeybook/presenter/ReadBookPresenterImpl.java
@@ -333,16 +333,7 @@ public class ReadBookPresenterImpl extends BasePresenterImpl<IReadBookView> impl
             if (!content.startsWith(bookShelf.getChapterList(chapterIndex).getDurChapterName())) {
                 content = String.format("%s\r\n%s", bookShelf.getChapterList(chapterIndex).getDurChapterName(), paragraphStr);
             }
-            //替换
-            if (ReplaceRuleManage.getEnabled() != null && ReplaceRuleManage.getEnabled().size() > 0) {
-                for (ReplaceRuleBean replaceRule : ReplaceRuleManage.getEnabled()) {
-                    try {
-                        content = content.replaceAll(replaceRule.getRegex(), replaceRule.getReplacement());
-                    } catch (Exception e1) {
-                        e1.printStackTrace();
-                    }
-                }
-            }
+            content = replaceContent(content);
             TextPaint mPaint = (TextPaint) mView.getPaint();
             mPaint.setSubpixelText(true);
             Layout tempLayout = new StaticLayout(content, mPaint, pageWidth, Layout.Alignment.ALIGN_NORMAL, 0, 0, false);
@@ -356,9 +347,45 @@ public class ReadBookPresenterImpl extends BasePresenterImpl<IReadBookView> impl
     }
 
     /**
+     * 替换净化
+     */
+    private String replaceContent(String content) {
+        String allLine[] = content.split("\r\n\u3000\u3000");
+        //替换
+        if (ReplaceRuleManage.getEnabled() != null && ReplaceRuleManage.getEnabled().size() > 0) {
+            StringBuilder contentBuilder = new StringBuilder();
+            for (String line : allLine) {
+                for (ReplaceRuleBean replaceRule : ReplaceRuleManage.getEnabled()) {
+                    try {
+                        line = line.replaceAll(replaceRule.getRegex(), replaceRule.getReplacement());
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                if (line.length() > 0) {
+                    if (contentBuilder.length() == 0) {
+                        contentBuilder.append(line);
+                    } else {
+                        contentBuilder.append("\r\n").append("\u3000\u3000").append(line);
+                    }
+                }
+            }
+            content = contentBuilder.toString();
+            for (ReplaceRuleBean replaceRule : ReplaceRuleManage.getEnabled()) {
+                if (replaceRule.getRegex().contains("\r\n")) {
+                    try {
+                        content = content.replaceAll(replaceRule.getRegex(), replaceRule.getReplacement());
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        }
+        return content;
+    }
+
+    /**
      * 设置每页行数
-     *
-     * @param pageLineCount
      */
     @Override
     public void setPageLineCount(int pageLineCount) {
