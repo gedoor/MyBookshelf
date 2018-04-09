d9157d62f56d5beb6ecc53a14d18867dd1325863
GKF <gekunfei@live.com>
2018-04-09 16:04:30 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/view/activity/ReadBookActivity.java
+++ app/src/main/java/com/monke/monkeybook/view/activity/ReadBookActivity.java
@@ -478,6 +478,7 @@ public class ReadBookActivity extends MBaseActivity<IReadBookPresenter> implemen
             @Override
             public void setFont(String path) {
                 csvBook.setFont();
+                csvBook.changeTextSize();
             }
 
             @Override
