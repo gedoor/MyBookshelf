acf74d422c223a1ab9656e8b89a7575b86eb75b2
1184313565@qq.com <lkmm118>
2018-04-09 12:50:33 +0800

    更换字体控件

--- app/src/main/java/com/monke/monkeybook/widget/ContentTextView.java
+++ app/src/main/java/com/monke/monkeybook/widget/ContentTextView.java
@@ -2,17 +2,23 @@ package com.monke.monkeybook.widget;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.support.v7.widget.AppCompatTextView;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 
+
+import android.graphics.Paint;
+
 /**
- * Created by ZQH on 2017/4/10.
- * 内容显示
+ * 解决文字排版混乱参差不齐的问题
  */
 
-public class ContentTextView extends android.support.v7.widget.AppCompatTextView {
+public class ContentTextView extends AppCompatTextView {
+
+    private int mLineY;
+    private int mViewWidth;
 
     public ContentTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -26,8 +32,90 @@ public class ContentTextView extends android.support.v7.widget.AppCompatTextView
     @Override
     protected void onDraw(Canvas canvas) {
         TextPaint paint = getPaint();
-        paint.setColor(getTextColors().getDefaultColor());
-        Layout layout = new StaticLayout(getText(), paint, canvas.getWidth(), Layout.Alignment.ALIGN_NORMAL, getLineSpacingMultiplier(), getLineSpacingExtra(), false);
-        layout.draw(canvas);
+        paint.setColor(getCurrentTextColor());
+        paint.drawableState = getDrawableState();
+        mViewWidth = getMeasuredWidth();
+        String text = getText().toString();
+        mLineY = 0;
+        mLineY += getTextSize();
+        Layout layout = getLayout();
+
+        if (layout == null) {
+            return;
+        }
+
+        Paint.FontMetrics fm = paint.getFontMetrics();
+        int textHeight = (int) (Math.ceil(fm.descent - fm.ascent));
+        textHeight = (int) (textHeight * layout.getSpacingMultiplier() + layout.getSpacingAdd());
+
+        //解决了最后一行文字间距过大的问题
+        for (int i = 0; i < layout.getLineCount(); i++) {
+            int lineStart = layout.getLineStart(i);
+            int lineEnd = layout.getLineEnd(i);
+            float width = StaticLayout.getDesiredWidth(text, lineStart, lineEnd, getPaint());
+            String line = text.substring(lineStart, lineEnd);
+
+            if (i < layout.getLineCount() - 1) {
+                if (needScale(line)) {
+                    drawScaledText(canvas, line, width);
+                } else {
+                    canvas.drawText(line, 0, mLineY, paint);
+                }
+            } else {
+                canvas.drawText(line, 0, mLineY, paint);
+            }
+
+            mLineY += textHeight;
+        }
+
     }
+
+    private void drawScaledText(Canvas canvas, String line, float lineWidth) {
+        float x = 0;
+        if (isFirstLineOfParagraph(line)) {
+            String blanks = " ";
+            canvas.drawText(blanks, x, mLineY, getPaint());
+            float bw = StaticLayout.getDesiredWidth(blanks, getPaint());
+            x += bw;
+
+            line = line.substring(3);
+        }
+
+
+        int gapCount = line.length() - 1;
+
+        int i = 0;
+
+        if (line.length() > 2 && line.charAt(0) == 12288 && line.charAt(1) == 12288) {
+            String substring = line.substring(0, 2);
+            float cw = StaticLayout.getDesiredWidth(substring, getPaint());
+            canvas.drawText(substring, x, mLineY, getPaint());
+            x += cw;
+            i += 2;
+        }
+
+        float d = (mViewWidth - lineWidth) / gapCount;
+        for (; i < line.length(); i++) {
+            String c = String.valueOf(line.charAt(i));
+            float cw = StaticLayout.getDesiredWidth(c, getPaint());
+            canvas.drawText(c, x, mLineY, getPaint());
+            x += cw + d;
+        }
+
+    }
+
+    private boolean isFirstLineOfParagraph(String line) {
+        return line.length() > 3 && line.charAt(0) == ' ' && line.charAt(1) == ' ';
+    }
+
+
+    private boolean needScale(String line) {
+        if (line == null || line.length() == 0) {
+            return false;
+        } else {
+            return line.charAt(line.length() - 1) != '\n';
+        }
+
+    }
+
 }
