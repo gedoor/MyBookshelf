88e640dd5874db21ba0e2e20f5ebf150942643c0
ttydy <yinfugang@gmail.com>
2018-05-08 20:14:14 +0800

    软件全沉浸式，有开头。（暂时要重启才能生效）
    加入点击菜单跳出虚拟按键，没有（上/下）一页时也会有虚拟按键。

--- app/src/main/java/com/monke/monkeybook/base/MBaseActivity.java
+++ app/src/main/java/com/monke/monkeybook/base/MBaseActivity.java
@@ -3,20 +3,33 @@ package com.monke.monkeybook.base;
 
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatDelegate;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.monke.basemvplib.BaseActivity;
 import com.monke.basemvplib.impl.IPresenter;
 import com.monke.monkeybook.R;
+import com.monke.monkeybook.utils.StatusBarCompat;
+import com.monke.monkeybook.utils.StatusBarUtil;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
+import static com.monke.monkeybook.utils.StatusBarCompat.getNavigationBarHeight;
+import static com.monke.monkeybook.utils.StatusBarCompat.getStatusBarHeight;
 
 public abstract class MBaseActivity<T extends IPresenter> extends BaseActivity<T> {
     public SharedPreferences preferences;
@@ -25,6 +38,11 @@ public abstract class MBaseActivity<T extends IPresenter> extends BaseActivity<T
     protected void onCreate(Bundle savedInstanceState) {
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         super.onCreate(savedInstanceState);
+
+        if (preferences.getBoolean("immersionStatusBar", false)) {
+            StatusBarCompat.compat(this, 0);
+        }
+
         setNightTheme();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             getWindow().getDecorView().setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_NO_EXCLUDE_DESCENDANTS);
