760c80f33ab2212c17bed144173fed8b7493528e
GKF <gekunfei@live.com>
2018-05-23 17:31:14 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/view/activity/ReadStyleActivity.java
+++ app/src/main/java/com/monke/monkeybook/view/activity/ReadStyleActivity.java
@@ -1,17 +1,35 @@
 package com.monke.monkeybook.view.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.monke.basemvplib.impl.IPresenter;
 import com.monke.monkeybook.R;
 import com.monke.monkeybook.base.MBaseActivity;
+import com.monke.monkeybook.widget.ContentTextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ReadStyleActivity extends MBaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tv_content)
+    ContentTextView tvContent;
+    @BindView(R.id.tvSelectTextColor)
+    TextView tvSelectTextColor;
+    @BindView(R.id.tvSelectBgColor)
+    TextView tvSelectBgColor;
+    @BindView(R.id.tvSelectBgImage)
+    TextView tvSelectBgImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_read_style);
     }
 
     /**
@@ -27,7 +45,10 @@ public class ReadStyleActivity extends MBaseActivity {
      */
     @Override
     protected void onCreateActivity() {
-
+        setContentView(R.layout.activity_read_style);
+        ButterKnife.bind(this);
+        this.setSupportActionBar(toolbar);
+        setupActionBar();
     }
 
     /**
@@ -38,4 +59,24 @@ public class ReadStyleActivity extends MBaseActivity {
 
     }
 
+    //设置ToolBar
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(R.string.about);
+        }
+    }
+
+    //菜单
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
