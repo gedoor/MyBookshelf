c083b085b2a4022330639de9521275ee91d419e6
GKF <gekunfei@live.com>
2018-06-01 09:34:48 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/view/activity/MainActivity.java
+++ app/src/main/java/com/monke/monkeybook/view/activity/MainActivity.java
@@ -202,6 +202,17 @@ public class MainActivity extends MBaseActivity<IMainPresenter> implements IMain
         };
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem pauseMenu = menu.getItem(6);
+        if (viewIsList) {
+            pauseMenu.setTitle(R.string.action_grid);
+        } else {
+            pauseMenu.setTitle(R.string.action_list);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
     // 添加菜单
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
