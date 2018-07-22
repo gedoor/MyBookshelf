fea98eafbcd3688d0a2da1533f9e3f64c638469b
gedoor <gekunfei@live.com>
2018-07-22 13:04:12 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/model/BookSourceManage.java
+++ app/src/main/java/com/monke/monkeybook/model/BookSourceManage.java
@@ -106,7 +106,7 @@ public class BookSourceManage extends BaseModelImpl {
             try {
                 List<BookSourceBean> bookSourceBeans = new Gson().fromJson(json, new TypeToken<List<BookSourceBean>>() {
                 }.getType());
-                int i = 1;
+                int i = 0;
                 for (BookSourceBean bookSourceBean : bookSourceBeans) {
                     if (Objects.equals(bookSourceBean.getBookSourceGroup(), "删除")) {
                         DbHelper.getInstance().getmDaoSession().getBookSourceBeanDao().queryBuilder()
