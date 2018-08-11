d08aaae25074b1c97caca2e831b0adadcb4e080c
gedoor <gekunfei@live.com>
2018-08-11 16:44:20 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/help/BookshelfHelp.java
+++ app/src/main/java/com/monke/monkeybook/help/BookshelfHelp.java
@@ -33,44 +33,34 @@ import java.util.List;
 
 public class BookshelfHelp {
     /**
-     * 根据文件名判断是否被缓存过 (因为可能数据库显示被缓存过，但是文件中却没有的情况，所以需要根据文件判断是否被缓存
-     * 过)
-     * @param folderName : bookId
-     * @param fileName: chapterName
-     * @return
+     * 根据文件名判断是否被缓存过 (因为可能数据库显示被缓存过，但是文件中却没有的情况，所以需要根据文件判断是否被缓存过)
      */
-    public static boolean isChapterCached(String folderName, String fileName){
+    public static boolean isChapterCached(String folderName, String fileName) {
         File file = new File(Constant.BOOK_CACHE_PATH + folderName
-                + File.separator + fileName + FileUtils.SUFFIX_NB);
+                + File.separator + fomartFileName(fileName) + FileUtils.SUFFIX_NB);
         return file.exists();
     }
 
     /**
      * 删除章节文件
-     * @param folderName : bookId
-     * @param fileName: chapterName
-     * @return
      */
-    public static void delChapter(String folderName, String fileName){
+    public static void delChapter(String folderName, String fileName) {
         FileUtils.deleteFile(Constant.BOOK_CACHE_PATH + folderName
-                + File.separator + fileName + FileUtils.SUFFIX_NB);
+                + File.separator + fomartFileName(fileName) + FileUtils.SUFFIX_NB);
     }
 
     /**
      * 存储章节
-     * @param folderName
-     * @param fileName
-     * @param content
      */
-    public static void saveChapterInfo(String folderName,String fileName,String content){
+    public static void saveChapterInfo(String folderName, String fileName, String content) {
         if (content == null) {
             content = "";
         }
-        File file = getBookFile(folderName, fileName);
+        File file = getBookFile(folderName, fomartFileName(fileName));
         //获取流并存储
         Writer writer = null;
         try {
-            writer = new BufferedWriter(new FileWriter(file));
+            writer = new BufferedWriter(new FileWriter(fomartFileName(fileName)));
             writer.write(content);
             writer.flush();
         } catch (IOException e) {
@@ -81,13 +71,14 @@ public class BookshelfHelp {
 
     /**
      * 创建或获取存储文件
-     * @param folderName
-     * @param fileName
-     * @return
      */
-    public static File getBookFile(String folderName, String fileName){
+    public static File getBookFile(String folderName, String fileName) {
         return FileUtils.getFile(Constant.BOOK_CACHE_PATH + folderName
-                + File.separator + fileName + FileUtils.SUFFIX_NB);
+                + File.separator + fomartFileName(fileName) + FileUtils.SUFFIX_NB);
+    }
+
+    private static String fomartFileName(String fileName) {
+        return fileName.replace("/", ".");
     }
 
     public static List<BookShelfBean> getAllBook() {
