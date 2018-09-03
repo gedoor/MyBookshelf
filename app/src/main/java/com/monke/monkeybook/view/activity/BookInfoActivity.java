e0612833ec076d13c02896a63b3e3adc252b8c40
GKF <gekunfei@live.com>
2018-09-03 10:23:49 +0800

    优化

---
+++ app/src/main/java/com/monke/monkeybook/view/activity/BookInfoActivity.java
@@ -0,0 +1,30 @@
+package com.monke.monkeybook.view.activity;
+
+import com.monke.basemvplib.impl.IPresenter;
+import com.monke.monkeybook.base.MBaseActivity;
+
+public class BookInfoActivity extends MBaseActivity{
+    /**
+     * P层绑定   若无则返回null;
+     */
+    @Override
+    protected IPresenter initInjector() {
+        return null;
+    }
+
+    /**
+     * 布局载入  setContentView()
+     */
+    @Override
+    protected void onCreateActivity() {
+
+    }
+
+    /**
+     * 数据初始化
+     */
+    @Override
+    protected void initData() {
+
+    }
+}
