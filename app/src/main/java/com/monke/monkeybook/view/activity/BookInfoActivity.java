142a68e190b1e92c4dc4ca11ccc10584f6e200e6
GKF <gekunfei@live.com>
2018-09-03 11:00:25 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/view/activity/BookInfoActivity.java
+++ app/src/main/java/com/monke/monkeybook/view/activity/BookInfoActivity.java
@@ -1,15 +1,45 @@
 package com.monke.monkeybook.view.activity;
 
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
 import com.monke.basemvplib.impl.IPresenter;
+import com.monke.monkeybook.R;
 import com.monke.monkeybook.base.MBaseActivity;
 
-public class BookInfoActivity extends MBaseActivity{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BookInfoActivity extends MBaseActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.iv_cover)
+    ImageView ivCover;
+    @BindView(R.id.tie_book_name)
+    TextInputEditText tieBookName;
+    @BindView(R.id.til_book_name)
+    TextInputLayout tilBookName;
+    @BindView(R.id.tie_book_author)
+    TextInputEditText tieBookAuthor;
+    @BindView(R.id.til_book_author)
+    TextInputLayout tilBookAuthor;
+
     /**
      * P层绑定   若无则返回null;
      */
     @Override
     protected IPresenter initInjector() {
         return null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_info);
+        ButterKnife.bind(this);
     }
 
     /**
