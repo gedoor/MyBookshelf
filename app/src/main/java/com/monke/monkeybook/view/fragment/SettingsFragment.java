fc7ae7c102526312469c5eafcc962a80e2155314
GKF <gekunfei@live.com>
2018-09-26 13:37:45 +0800

    自定义缓存路径

--- app/src/main/java/com/monke/monkeybook/view/fragment/SettingsFragment.java
+++ app/src/main/java/com/monke/monkeybook/view/fragment/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.monke.monkeybook.view.fragment;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,13 +8,16 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 
 import com.hwangjr.rxbus.RxBus;
+import com.monke.monkeybook.MApplication;
 import com.monke.monkeybook.R;
-import com.monke.monkeybook.bean.BookShelfBean;
+import com.monke.monkeybook.help.FileHelp;
 import com.monke.monkeybook.help.RxBusTag;
 import com.monke.monkeybook.view.activity.SettingActivity;
+
+import pub.devrel.easypermissions.EasyPermissions;
 
 /**
  * Created by GKF on 2017/12/16.
@@ -21,8 +25,10 @@ import com.monke.monkeybook.view.activity.SettingActivity;
  */
 
 public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private final int REQUEST_CODE_OPEN_DIRECTORY = 101;
     private SettingActivity settingActivity;
     private Context mContext;
+    private SharedPreferences sharedPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,8 +37,14 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         addPreferencesFromResource(R.xml.pref_settings);
         mContext = this.getActivity();
         settingActivity = (SettingActivity) this.getActivity();
-
-        bindPreferenceSummaryToValue(findPreference(mContext.getString(R.string.pk_bookshelf_px)));
+        sharedPreferences = getPreferenceManager().getSharedPreferences();
+        if (sharedPreferences.getString(getString(R.string.pk_download_path), "").equals("")) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(getString(R.string.pk_download_path), FileHelp.getCachePath());
+            editor.apply();
+        }
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pk_bookshelf_px)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pk_download_path)));
     }
 
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = (Preference preference, Object value)-> {
@@ -76,6 +88,34 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
             RxBus.get().post(RxBusTag.IMMERSION_CHANGE, true);
         } else if (key.equals(getString(R.string.pk_bookshelf_px))) {
             RxBus.get().post(RxBusTag.UPDATE_PX, true);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference.getKey().equals(getString(R.string.pk_download_path))) {
+            if (!EasyPermissions.hasPermissions(mContext, MApplication.PerList)) {
+                EasyPermissions.requestPermissions(this, "自定义缓存路径需要存储权限", 0, MApplication.PerList);
+                return true;
+            }
+//            Intent intent = null;
+//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+//                intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+//                startActivityForResult(intent, REQUEST_CODE_OPEN_DIRECTORY);
+//            }
+
+
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_OPEN_DIRECTORY && resultCode == Activity.RESULT_OK) {
+
         }
     }
 }
