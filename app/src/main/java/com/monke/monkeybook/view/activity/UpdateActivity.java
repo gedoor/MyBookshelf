d338efb075957fc54ecd590d117f8cccc4feec41
gedoor <gekunfei@live.com>
2018-10-02 13:21:11 +0800

    优化

---
+++ app/src/main/java/com/monke/monkeybook/view/activity/UpdateActivity.java
@@ -0,0 +1,22 @@
+package com.monke.monkeybook.view.activity;
+
+import com.monke.basemvplib.impl.IPresenter;
+import com.monke.monkeybook.base.MBaseActivity;
+
+public class UpdateActivity extends MBaseActivity {
+
+    @Override
+    protected IPresenter initInjector() {
+        return null;
+    }
+
+    @Override
+    protected void onCreateActivity() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
