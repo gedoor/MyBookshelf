2bc3e56a13d4a94279567c4aae6dbe1ea171c5c7
GKF <gekunfei@live.com>
2018-10-23 13:11:55 +0800

    优化

---
+++ app/src/main/java/com/monke/monkeybook/widget/FilletImageView.java
@@ -0,0 +1,57 @@
+package com.monke.monkeybook.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class FilletImageView extends android.support.v7.widget.AppCompatImageView {
+    float width,height;
+
+    public FilletImageView(Context context) {
+        super(context);
+    }
+
+    public FilletImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FilletImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (Build.VERSION.SDK_INT < 18) {
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        width = getWidth();
+        height = getHeight();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (width >= 12 && height > 12) {
+            Path path = new Path();
+            //四个圆角
+            path.moveTo(12, 0);
+            path.lineTo(width - 12, 0);
+            path.quadTo(width, 0, width, 12);
+            path.lineTo(width, height - 12);
+            path.quadTo(width, height, width - 12, height);
+            path.lineTo(12, height);
+            path.quadTo(0, height, 0, height - 12);
+            path.lineTo(0, 12);
+            path.quadTo(0, 0, 12, 0);
+
+            canvas.clipPath(path);
+        }
+        super.onDraw(canvas);
+    }
+
+
+}
