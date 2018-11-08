7608bd89d8b343423bd3c6b6fb86d9b64a9d145d
GKF <gekunfei@live.com>
2018-11-08 15:31:00 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/widget/FilletImageView.java
+++ app/src/main/java/com/monke/monkeybook/widget/FilletImageView.java
@@ -1,5 +1,6 @@
 package com.monke.monkeybook.widget;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Path;
@@ -30,18 +31,19 @@ public class FilletImageView extends android.support.v7.widget.AppCompatImageVie
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (width >= 12 && height > 12) {
+        if (width >= 10 && height > 10) {
+            @SuppressLint("DrawAllocation")
             Path path = new Path();
             //四个圆角
-            path.moveTo(12, 0);
-            path.lineTo(width - 12, 0);
-            path.quadTo(width, 0, width, 12);
-            path.lineTo(width, height - 12);
-            path.quadTo(width, height, width - 12, height);
-            path.lineTo(12, height);
-            path.quadTo(0, height, 0, height - 12);
-            path.lineTo(0, 12);
-            path.quadTo(0, 0, 12, 0);
+            path.moveTo(10, 0);
+            path.lineTo(width - 10, 0);
+            path.quadTo(width, 0, width, 10);
+            path.lineTo(width, height - 10);
+            path.quadTo(width, height, width - 10, height);
+            path.lineTo(10, height);
+            path.quadTo(0, height, 0, height - 10);
+            path.lineTo(0, 10);
+            path.quadTo(0, 0, 10, 0);
 
             canvas.clipPath(path);
         }
