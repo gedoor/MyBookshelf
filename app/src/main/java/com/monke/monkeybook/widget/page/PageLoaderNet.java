8a035bbd15763bfd638f6c29fde3965e1ef092ca
GKF <gekunfei@live.com>
2018-11-13 13:22:08 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/widget/page/PageLoaderNet.java
+++ app/src/main/java/com/monke/monkeybook/widget/page/PageLoaderNet.java
@@ -135,8 +135,7 @@ public class PageLoaderNet extends PageLoader {
                         }
 
                         @Override
-                        public void onComplete() {
-                        }
+                        public void onComplete() { }
                     });
         }
     }
