1fd5e969967db1223ea0acd3e9557e5022ed7a3c
GKF <gekunfei@live.com>
2018-11-30 09:24:56 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/widget/ScrollTextView.java
+++ app/src/main/java/com/monke/monkeybook/widget/ScrollTextView.java
@@ -57,7 +57,7 @@ public class ScrollTextView extends AppCompatTextView {
     public boolean dispatchTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN)
             //如果是新的按下事件，则对mBottomFlag重新初始化
-            mBottomFlag = false;
+            mBottomFlag = mOffsetHeight <= 0;
         //如果已经不要这次事件，则传出取消的信号，这里的作用不大
         if (mBottomFlag)
             event.setAction(MotionEvent.ACTION_CANCEL);
