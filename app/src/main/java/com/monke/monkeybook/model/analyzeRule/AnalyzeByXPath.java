46fc7abf458ffc95f9502f316d3dd5a767734c03
GKF <gekunfei@live.com>
2018-12-07 10:01:27 +0800

    优化

--- app/src/main/java/com/monke/monkeybook/model/analyzeRule/AnalyzeByXPath.java
+++ app/src/main/java/com/monke/monkeybook/model/analyzeRule/AnalyzeByXPath.java
@@ -1,5 +1,29 @@
 package com.monke.monkeybook.model.analyzeRule;
 
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.seimicrawler.xpath.JXDocument;
+
+import java.util.List;
+
 public class AnalyzeByXPath {
+    JXDocument jxDocument;
+
+    AnalyzeByXPath(Elements doc) {
+        jxDocument = new JXDocument(doc);
+    }
+
+    public Elements getElements(String xPath) {
+        Elements elements = new Elements();
+        List<Object> objects = jxDocument.sel(xPath);
+        for (Object object : objects) {
+            if (object instanceof Element) {
+                elements.add((Element) object);
+            }
+        }
+        return elements;
+    }
+
+
 
 }
