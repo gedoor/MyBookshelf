1081575b922f67a38610e71ca8994cdc0444663d
gedoor <gekunfei@live.com>
2018-12-12 20:56:03 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/help/ReadBookControl.java
+++ app/src/main/java/com/kunfei/bookshelf/help/ReadBookControl.java
@@ -3,12 +3,13 @@ package com.kunfei.bookshelf.help;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.util.DisplayMetrics;
 
 import com.kunfei.bookshelf.MApplication;
 import com.kunfei.bookshelf.utils.BitmapUtil;
@@ -175,7 +176,11 @@ public class ReadBookControl {
             if (getBgCustom(textDrawableIndex) == 2 && getBgPath(textDrawableIndex) != null) {
                 bgIsColor = false;
                 String bgPath = getBgPath(textDrawableIndex);
-                bgBitmap = BitmapFactory.decodeFile(bgPath);
+                Resources resources = MApplication.getInstance().getResources();
+                DisplayMetrics dm = resources.getDisplayMetrics();
+                int width = dm.widthPixels;
+                int height = dm.heightPixels;
+                bgBitmap = BitmapUtil.getFitSampleBitmap(bgPath, width, height);
                 return;
             } else if (getBgCustom(textDrawableIndex) == 1) {
                 bgIsColor = true;
