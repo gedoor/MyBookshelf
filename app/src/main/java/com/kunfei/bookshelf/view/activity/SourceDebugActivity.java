d027b26518378b531ca534d7e3bf24631322ff6b
gedoor <gekunfei@live.com>
2018-12-14 22:09:17 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/view/activity/SourceDebugActivity.java
+++ app/src/main/java/com/kunfei/bookshelf/view/activity/SourceDebugActivity.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -81,6 +84,34 @@ public class SourceDebugActivity extends MBaseActivity {
         setContentView(R.layout.activity_source_debug);
         ButterKnife.bind(this);
         this.setSupportActionBar(toolbar);
+        setupActionBar();
+    }
+
+    //设置ToolBar
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    // 添加菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_source_login, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //菜单
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
