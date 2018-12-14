148645f8b492d26cf24466f87f6e63bfd62a8f9f
gedoor <gekunfei@live.com>
2018-12-14 22:16:42 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/view/activity/SourceDebugActivity.java
+++ app/src/main/java/com/kunfei/bookshelf/view/activity/SourceDebugActivity.java
@@ -5,15 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.util.TypedValue;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.kunfei.basemvplib.impl.IPresenter;
@@ -35,8 +30,6 @@ public class SourceDebugActivity extends MBaseActivity {
 
     @BindView(R.id.searchView)
     SearchView searchView;
-    @BindView(R.id.card_search)
-    CardView cardSearch;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.action_bar)
@@ -95,13 +88,6 @@ public class SourceDebugActivity extends MBaseActivity {
         }
     }
 
-    // 添加菜单
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_source_login, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
     //菜单
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -124,22 +110,8 @@ public class SourceDebugActivity extends MBaseActivity {
     }
 
     private void initSearchView() {
-        SearchView.SearchAutoComplete mSearchAutoComplete = searchView.findViewById(R.id.search_src_text);
         searchView.setQueryHint(getString(R.string.search_book_key));
-        //获取到TextView的控件
-        mSearchAutoComplete.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
-        mSearchAutoComplete.setPadding(15, 0, 0, 0);
         searchView.onActionViewExpanded();
-        LinearLayout editFrame = searchView.findViewById(android.support.v7.appcompat.R.id.search_edit_frame);
-        ImageView closeButton = searchView.findViewById(android.support.v7.appcompat.R.id.search_close_btn);
-        ImageView goButton = searchView.findViewById(android.support.v7.appcompat.R.id.search_go_btn);
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) editFrame.getLayoutParams();
-        params.setMargins(20, 0, 10, 0);
-        editFrame.setLayoutParams(params);
-        closeButton.setScaleX(0.9f);
-        closeButton.setScaleY(0.9f);
-        closeButton.setPadding(0, 0, 0, 0);
-        goButton.setPadding(0, 0, 0, 0);
         searchView.setSubmitButtonEnabled(true);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
