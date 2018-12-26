ce3288358a2fb4deb57176ac2ac9a5b4342910d5
GKF <gekunfei@live.com>
2018-12-26 11:12:31 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/view/activity/MainActivity.java
+++ app/src/main/java/com/kunfei/bookshelf/view/activity/MainActivity.java
@@ -181,6 +181,8 @@ public class MainActivity extends BaseTabActivity<MainContract.Presenter> implem
             }
             View customView = tab.getCustomView();
             if (customView == null) return;
+            TextView tv = customView.findViewById(R.id.tabtext);
+            tab.setContentDescription(String.format("%s,%s", tv.getText(), "选中时点击可弹出菜单"));
             ImageView im = customView.findViewById(R.id.tabicon);
             if (tab.isSelected()) {
                 im.setVisibility(View.VISIBLE);
@@ -269,6 +271,7 @@ public class MainActivity extends BaseTabActivity<MainContract.Presenter> implem
         if (customView == null) return;
         TextView tv = customView.findViewById(R.id.tabtext);
         tv.setText(getResources().getStringArray(R.array.book_group_array)[group]);
+        tab.setContentDescription(String.format("%s,%s", tv.getText(), "选中时再次点击可切换书架"));
     }
 
     private View tab_icon(String name, Integer iconID) {
