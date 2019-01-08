b79ffc655214e692dc2d7f4ef903749f1d72e6be
gedoor <gekunfei@live.com>
2019-01-08 20:06:55 +0800

    优化

---
+++ app/src/main/java/com/kunfei/bookshelf/utils/Theme/prefs/ATEPreferenceCategory.java
@@ -0,0 +1,41 @@
+package com.kunfei.bookshelf.utils.Theme.prefs;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.preference.PreferenceCategory;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import com.kunfei.bookshelf.utils.Theme.ThemeStore;
+
+public class ATEPreferenceCategory extends PreferenceCategory {
+
+    @SuppressLint("NewApi")
+    public ATEPreferenceCategory(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public ATEPreferenceCategory(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ATEPreferenceCategory(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ATEPreferenceCategory(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        if (view instanceof TextView) {
+            TextView tv = (TextView) view;
+            tv.setTextColor(ThemeStore.accentColor(view.getContext()));//设置title文本的颜色
+        }
+    }
+
+}
