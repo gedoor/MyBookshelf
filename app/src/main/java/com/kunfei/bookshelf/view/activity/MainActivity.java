0bae9676a40810dc25bf22461f2f9bd15253f3ff
GKF <gekunfei@live.com>
2019-01-15 10:03:50 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/view/activity/MainActivity.java
+++ app/src/main/java/com/kunfei/bookshelf/view/activity/MainActivity.java
@@ -676,6 +676,9 @@ public class MainActivity extends BaseTabActivity<MainContract.Presenter> implem
         }
     }
 
+    /**
+     * 退出
+     */
     public void exit() {
         if ((System.currentTimeMillis() - exitTime) > 2000) {
             showSnackBar(toolbar, "再按一次退出程序");
