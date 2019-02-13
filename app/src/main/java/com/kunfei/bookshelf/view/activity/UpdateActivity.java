5176157372150e630fe06d1a5caeab8f0de263cf
GKF <gekunfei@live.com>
2019-02-13 15:12:13 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/view/activity/UpdateActivity.java
+++ app/src/main/java/com/kunfei/bookshelf/view/activity/UpdateActivity.java
@@ -2,7 +2,6 @@ package com.kunfei.bookshelf.view.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
