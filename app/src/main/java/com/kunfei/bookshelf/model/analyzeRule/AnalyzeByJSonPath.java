27340c16cd03c073ab0289308db17aa84ed39ede
GKF <gekunfei@live.com>
2019-03-14 08:15:21 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/model/analyzeRule/AnalyzeByJSonPath.java
+++ app/src/main/java/com/kunfei/bookshelf/model/analyzeRule/AnalyzeByJSonPath.java
@@ -37,7 +37,7 @@ public class AnalyzeByJSonPath {
             elementsType = "|";
         }
         if (rules.length == 1) {
-            if (!rule.contains("{")) {
+            if (!rule.contains("{$")) {
                 try {
                     Object object = ctx.read(rule);
                     if (object instanceof List) {
