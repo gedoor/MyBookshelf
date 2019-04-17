9d87aeaaddc75f62baf4d401918a30f82a5e9620
gedoor <gekunfei@live.com>
2019-04-17 22:56:51 +0800

    添加web编辑书源

---
+++ app/src/main/java/com/kunfei/bookshelf/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.kunfei.bookshelf.controller;
+
+import com.yanzhenjie.andserver.annotation.Controller;
+import com.yanzhenjie.andserver.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping(path = "/")
+    public String index() {
+        // Equivalent to [return "/index"].
+        return "forward:/index.html";
+    }
+}
