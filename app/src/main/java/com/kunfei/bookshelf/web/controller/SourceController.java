c88aa568052f86cbf432514d50dc7b5cd10b1f11
GKF <gekunfei@live.com>
2019-04-18 15:27:32 +0800

    web写源

--- app/src/main/java/com/kunfei/bookshelf/web/controller/SourceController.java
+++ app/src/main/java/com/kunfei/bookshelf/web/controller/SourceController.java
@@ -9,24 +9,21 @@ import com.kunfei.bookshelf.model.BookSourceManager;
 import com.yanzhenjie.andserver.annotation.PostMapping;
 import com.yanzhenjie.andserver.annotation.RestController;
 import com.yanzhenjie.andserver.http.RequestBody;
+import com.yanzhenjie.andserver.util.MediaType;
 
 import java.io.IOException;
 
 @RestController
 class SourceController {
 
-    @PostMapping(path = "/saveSource", consumes = "application/json")
-    String saveSource(RequestBody body) {
-        try {
-            String json = body.string();
-            BookSourceBean bookSourceBean = new Gson().fromJson(json, new TypeToken<BookSourceBean>() {
-            }.getType());
-            if (TextUtils.isEmpty(bookSourceBean.getBookSourceName()) || TextUtils.isEmpty(bookSourceBean.getBookSourceUrl()))
-                return "书源名称和URL不能为空";
-            BookSourceManager.addBookSource(bookSourceBean);
-        } catch (IOException e) {
-            return e.getMessage();
-        }
+    @PostMapping(path = "/saveSource", consumes = MediaType.APPLICATION_JSON_VALUE)
+    String saveSource(RequestBody body) throws IOException {
+        String json = body.string();
+        BookSourceBean bookSourceBean = new Gson().fromJson(json, new TypeToken<BookSourceBean>() {
+        }.getType());
+        if (TextUtils.isEmpty(bookSourceBean.getBookSourceName()) || TextUtils.isEmpty(bookSourceBean.getBookSourceUrl()))
+            return "书源名称和URL不能为空";
+        BookSourceManager.addBookSource(bookSourceBean);
         return "保存成功";
     }
 
