fef9f2d4179a62ef2e63584c88c9fb0a351b5792
Antecer <ymh441915964@gmail.com>
2019-04-19 00:58:28 +0800

    优化"书源编辑器" 1.添加批量推送&拉取按钮,添加调试按钮等等.. 2.尝试添加书源联调功能

--- app/src/main/java/com/kunfei/bookshelf/web/controller/SourceController.java
+++ app/src/main/java/com/kunfei/bookshelf/web/controller/SourceController.java
@@ -4,6 +4,7 @@ import android.text.TextUtils;
 
 import com.kunfei.bookshelf.bean.BookSourceBean;
 import com.kunfei.bookshelf.model.BookSourceManager;
+import com.kunfei.bookshelf.utils.TimeUtils;
 import com.kunfei.bookshelf.web.model.ReturnData;
 import com.yanzhenjie.andserver.annotation.PostMapping;
 import com.yanzhenjie.andserver.annotation.RequestBody;
@@ -11,22 +12,22 @@ import com.yanzhenjie.andserver.annotation.ResponseBody;
 import com.yanzhenjie.andserver.annotation.RestController;
 import com.yanzhenjie.andserver.util.MediaType;
 
+import static com.kunfei.bookshelf.model.content.Debug.DEBUG_TIME_FORMAT;
+
 @RestController
 class SourceController {
 
     @ResponseBody
     @PostMapping(path = "/saveSource", consumes = MediaType.APPLICATION_JSON_VALUE)
-    ReturnData saveSource(@RequestBody BookSourceBean bookSourceBean) {
+    Object saveSource(@RequestBody BookSourceBean bookSourceBean) {
         ReturnData returnData = new ReturnData();
         if (TextUtils.isEmpty(bookSourceBean.getBookSourceName()) || TextUtils.isEmpty(bookSourceBean.getBookSourceUrl())) {
             returnData.setSuccess(false);
-            returnData.setData("书源名称和URL不能为空");
+            returnData.setErrorMsg("书源名称和URL不能为空");
             return returnData;
         }
         BookSourceManager.addBookSource(bookSourceBean);
-        returnData.setSuccess(true);
-        returnData.setData("保存成功");
-        return returnData;
+        return "保存成功";
     }
 
 }
