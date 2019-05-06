85ade8eb1b406e69ff9bea795cd3fd4495b18c95
gedoor <gekunfei@live.com>
2019-05-06 23:01:34 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/widget/page/PageLoader.java
+++ app/src/main/java/com/kunfei/bookshelf/widget/page/PageLoader.java
@@ -289,6 +289,7 @@ public abstract class PageLoader {
         mBatteryPaint.setAntiAlias(true);
         mBatteryPaint.setDither(true);
         mBatteryPaint.setTextSize(ScreenUtils.spToPx(DEFAULT_TIP_SIZE - 3));
+        mBatteryPaint.setTypeface(Typeface.createFromAsset(mContext.getAssets(), "number.ttf"));
 
         setupTextInterval();
         // 初始化页面样式
@@ -945,7 +946,7 @@ public abstract class PageLoader {
             Paint.FontMetrics fontMetrics = mBatteryPaint.getFontMetrics();
             String batteryLevel = String.valueOf(mBatteryLevel);
             float batTextLeft = outFrameLeft + (outFrameWidth - mBatteryPaint.measureText(batteryLevel)) / 2;
-            float batTextBaseLine = tipBottomBot - outFrameHeight / 2f - fontMetrics.top / 2 - fontMetrics.bottom / 2;
+            float batTextBaseLine = visibleBottom - outFrameHeight / 2f - fontMetrics.top / 2 - fontMetrics.bottom / 2;
             canvas.drawText(batteryLevel, batTextLeft, batTextBaseLine, mBatteryPaint);
         }
     }
