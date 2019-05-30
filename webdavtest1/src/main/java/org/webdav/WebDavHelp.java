ab37b3d80cb369726689c4b42042def31772b0c9
seanyzhou <seanyzhou100@gmail.com>
2019-05-30 18:53:07 +0800

    1

--- webdavtest1/src/main/java/org/webdav/WebDavHelp.java
+++ webdavtest1/src/main/java/org/webdav/WebDavHelp.java
@@ -5,28 +5,12 @@ import org.webdav.http.WAuth;
 public class WebDavHelp {
 
     public static String getWebDavUrl() {
-        /*String url = MApplication.getConfigPreferences().getString("web_dav_url", DEFAULT_WEB_DAV_URL);
-        assert url != null;
-        if (!url.endsWith("/")) {
-            url = url + "/";
-        }
-        return url;*/
-//        return "https://dav.jianguoyun.com/dav";
         return "https://dav.box.com/dav";
-//        return "https://d.docs.live.net/E7D70445555B5ACB";
     }
 
     public static boolean initWebDav() {
-//        String account = MApplication.getConfigPreferences().getString("web_dav_account", "");
-//        String password = MApplication.getConfigPreferences().getString("web_dav_password", "");
-//        if (!StringUtils.isTrimEmpty(account) && !StringUtils.isTrimEmpty(password)) {
-//            HttpAuth.setAuth(account, password);
-//            WAuth.setAuth("1872079@qq.com", "aaaaaa");
-            WAuth.setAuth("moonplusreader@gmail.com", "aa111111");
-//            WAuth.setAuth("1872079@qq.com", "th123456");
-            return true;
-//        }
-//        return false;
+        WAuth.setAuth("", "");
+        return true;
     }
 
 }
