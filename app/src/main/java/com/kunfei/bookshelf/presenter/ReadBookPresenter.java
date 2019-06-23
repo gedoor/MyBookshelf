cd33dc480f9d73edea719f45ffaeb0ed59de976f
kunfei <gekunfei@live.com>
2019-06-23 21:49:32 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/presenter/ReadBookPresenter.java
+++ app/src/main/java/com/kunfei/bookshelf/presenter/ReadBookPresenter.java
@@ -82,11 +82,6 @@ public class ReadBookPresenter extends BasePresenterImpl<ReadBookContract.View> 
                 if (!isEmpty(bookKey)) {
                     bookShelf = (BookShelfBean) BitIntentDataManager.getInstance().getData(bookKey);
                 }
-                String chapterListKey = intent.getStringExtra("chapterListKey");
-                if (!isEmpty(chapterListKey)) {
-                    chapterBeanList = (List<BookChapterBean>) BitIntentDataManager.getInstance().getData(chapterListKey);
-                    if (chapterBeanList == null) chapterBeanList = new ArrayList<>();
-                }
             }
             if (bookShelf == null && !isEmpty(mView.getNoteUrl())) {
                 bookShelf = BookshelfHelp.getBook(mView.getNoteUrl());
