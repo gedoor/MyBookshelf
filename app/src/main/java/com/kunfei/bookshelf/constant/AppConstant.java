4303d43e4a02477f63c24d9f27b2b65ec710a5f5
kunfei <gekunfei@live.com>
2019-07-17 16:40:04 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/constant/AppConstant.java
+++ app/src/main/java/com/kunfei/bookshelf/constant/AppConstant.java
@@ -32,7 +32,7 @@ public class AppConstant {
 
     public static final String DEFAULT_WEB_DAV_URL = "https://dav.jianguoyun.com/dav/";
 
-    public static final String DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3100.0 Safari/537.36";
+    public static final String DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36";
 
     public static final Pattern JS_PATTERN = Pattern.compile("(<js>[\\w\\W]*?</js>|@js:[\\w\\W]*$)", Pattern.CASE_INSENSITIVE);
     public static final Pattern EXP_PATTERN = Pattern.compile("\\{\\{([\\w\\W]*?)\\}\\}");
