bdd8bba1f97ad6f0eb8d1e56d6888c93209448a3
gedoor <gekunfei@live.com>
2020-08-07 16:48:04 +0800

    优化

--- app/src/main/java/com/kunfei/bookshelf/view/activity/WelcomeActivity.java
+++ app/src/main/java/com/kunfei/bookshelf/view/activity/WelcomeActivity.java
@@ -6,6 +6,7 @@ import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.kunfei.basemvplib.impl.IPresenter;
 import com.kunfei.bookshelf.DbHelper;
@@ -21,6 +22,8 @@ public class WelcomeActivity extends MBaseActivity<IPresenter> {
 
     @BindView(R.id.iv_bg)
     ImageView ivBg;
+    @BindView(R.id.tv_gzh)
+    TextView tvGzh;
 
     @Override
     protected IPresenter initInjector() {
@@ -35,6 +38,7 @@ public class WelcomeActivity extends MBaseActivity<IPresenter> {
             return;
         }
         setContentView(R.layout.activity_welcome);
+        tvGzh.setTextColor(ThemeStore.accentColor(this));
         AsyncTask.execute(DbHelper::getDaoSession);
         ButterKnife.bind(this);
         ivBg.setColorFilter(ThemeStore.accentColor(this));
